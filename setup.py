5de99ca782b4df2f8ac48999023495f19084c2cc
Arnaud Cornet <arnaud.cornet@gmail.com>
2018-11-11 09:52:07 -0500

    Make this a python module.
    Add the python package boilerplate.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='noway',
+      version='0.1',
+      description='Simple API wrapper around scaleway API',
+      url='http://github.com/emergencybutter/noway',
+      author='Arnaud Cornet',
+      author_email='arnaud.cornet@gmail.com',
+      license='MIT',
+      packages=['noway'],
+      zip_safe=False)
