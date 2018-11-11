7c701b7abd1278afed29dfefb7cbc76fc10572dc
Arnaud Cornet <arnaud.cornet@gmail.com>
2018-11-11 10:44:52 -0500

    Add retrying dependency.
    Add missing dependency in setup.py.
    Fix-up package email address.

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(name='noway',
       description='Simple API wrapper around scaleway API',
       url='http://github.com/emergencybutter/noway',
       author='Arnaud Cornet',
-      author_email='arnaud.cornet@gmail.com',
+      author_email='arnaud.cornet+noway@gmail.com',
       license='MIT',
       packages=['noway'],
-      zip_safe=False)
+      install_requires=[
+          'retrying',
+      ])
